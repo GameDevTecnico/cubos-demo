6edcc8299fcac35eb5b10f1d3d940e250f554629
fallenatlas <tiago.a.p.antunes@gmail.com>
2023-05-28 12:35:51 +0100

    feat: update engine and base code

--- src/components.hpp
+++ src/components.hpp
@@ -1,7 +1,4 @@
-#ifndef COMPONENTS_CAR_HPP
-#define COMPONENTS_CAR_HPP
-
-#include <components/base.hpp>
+#pragma once
 
 #include <glm/glm.hpp>
 
@@ -10,5 +7,3 @@ struct [[cubos::component("car", VecStorage)]] Car
     glm::vec3 vel = {1.0f, 0.0f, 0.0f};
     float angVel = 1.0f;
 };
-
-#endif // COMPONENTS_CAR_HPP
