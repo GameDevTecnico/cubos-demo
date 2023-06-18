fa309a7d8fb839452d072ed71997acd2167b72da
fallenatlas <tiago.a.p.antunes@gmail.com>
2023-06-18 18:11:00 +0100

    feat: add colliders, scene, engine

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,23 @@
 #include <cubos/engine/transform/plugin.hpp>
 #include <cubos/engine/voxels/plugin.hpp>
 #include <cubos/engine/input/plugin.hpp>
+#include <cubos/engine/scene/plugin.hpp>
 
 #include <vector>
 #include <cmath>
+
+//  aahhhhhh
+
+#include <cubos/core/geom/box.hpp>
+#include <cubos/core/gl/debug.hpp>
+#include <cubos/core/log.hpp>
+
+#include <cubos/engine/collisions/aabb.hpp>
+#include <cubos/engine/collisions/broad_phase_collisions.hpp>
+#include <cubos/engine/collisions/colliders/box.hpp>
+#include <cubos/engine/collisions/plugin.hpp>
+
+// ------------
 
 #include "cameraComponent.hpp"
 #include "components.hpp"
@@ -28,6 +42,7 @@ using namespace cubos::engine;
 static const Asset<Grid> CarAsset = AnyAsset("059c16e7-a439-44c7-9bdc-6e069dba0c75");
 static const Asset<Grid> TrackAsset = AnyAsset("059c16e7-a439-44c7-9bdc-6e069dba0c76");
 static const Asset<Palette> PaletteAsset = AnyAsset("1aa5e234-28cb-4386-99b4-39386b0fc215");
+static const Asset<Scene> SceneAsset = AnyAsset("059c16e7-a439-44c7-9bdc-6e069dba0c77");
 static const Asset<InputBindings> Player0BindingsAsset = AnyAsset("bf49ba61-5103-41bc-92e0-8a442d7842c3");
 static const Asset<InputBindings> Player1BindingsAsset = AnyAsset("bf49ba61-5103-41bc-92e0-8a442d7842c4");
 
@@ -75,6 +90,13 @@ static void setup(Commands cmds, Write<Assets> assets, Write<Renderer> renderer,
 {
     // Load the palette asset and add two colors to it.
     auto palette = assets->write(PaletteAsset);
+    for(int i = 1; i <= palette->size(); i++) {
+        CUBOS_INFO("Palette: {}, {}", i, Debug(palette->get(i)));
+    }
+    palette->set(6, {{10.0F, 0.1F, 0.1F, 1.0F}});
+
+    auto scene = assets->read(SceneAsset);
+    cmds.spawn(scene->blueprint);
     //auto black = palette->add({{0.1F, 0.1F, 0.1F, 1.0F}});
     //auto white = palette->add({{0.9F, 0.9F, 0.9F, 1.0F}});
 
@@ -101,7 +123,7 @@ static void setup(Commands cmds, Write<Assets> assets, Write<Renderer> renderer,
     trackOffset.y = -2.0F;
 
     // Spawn the floor entity.
-    cmds.create(RenderableGrid{TrackAsset, trackOffset}, LocalToWorld{}, Scale{4.0F});
+    //cmds.create(RenderableGrid{TrackAsset, trackOffset}, LocalToWorld{}, Scale{4.0F});
 
     // Spawn the camera entity.
     activeCameras->entities[0] =
@@ -137,17 +159,25 @@ static void setup(Commands cmds, Write<Assets> assets, Write<Renderer> renderer,
     //env->skyGradient[1] = {0.3F, 0.0F, 0.8F};
 }
 
-static void spawnCar(Commands cmds, Write<Assets> assets) {
+static void spawnCar(Commands cmds, Write<Assets> assets, Write<BroadPhaseCollisions> collisions) {
     auto car = assets->read(CarAsset);
     glm::vec3 offset = glm::vec3(car->size().x, 0.0F, car->size().z) / -2.0F;
 
-    cmds.create(Car{0,}, RenderableGrid{CarAsset, offset}, LocalToWorld{})
+    auto entity1 = cmds.create(Car{0,}, RenderableGrid{CarAsset, offset}, LocalToWorld{})
         .add(Position{{320.0F, 0.0F, 636.0F}})
-        .add(Rotation{glm::quat(glm::vec3(0, glm::radians(90.0F), 0))});
+        .add(Rotation{glm::quat(glm::vec3(0, glm::radians(90.0F), 0))})
+        .add(BoxCollider{.shape = {{6.0, 6.0, 14.0}}})
+        .entity();
 
-    cmds.create(Car{1,}, RenderableGrid{CarAsset, offset}, LocalToWorld{})
+    collisions->addEntity(entity1);
+
+    auto entity2 = cmds.create(Car{1,}, RenderableGrid{CarAsset, offset}, LocalToWorld{})
         .add(Position{{320.0F, 0.0F, 667.0F}})
-        .add(Rotation{glm::quat(glm::vec3(0, glm::radians(90.0F), 0))});
+        .add(Rotation{glm::quat(glm::vec3(0, glm::radians(90.0F), 0))})
+        .add(BoxCollider{.shape = {{6.0, 6.0, 14.0}}})
+        .entity();
+        
+    collisions->addEntity(entity2);
 
     cmds.create(SpotLight{.color = {1.0F, 1.0F, 1.0F}, .intensity = 4.0F, .range = 200.0F, .spotAngle = 70.0F, .innerSpotAngle = 60.0F}, LocalToWorld{})
         .add(Position{{340.0F, 10.0F, 636.0F}})
@@ -249,7 +279,7 @@ static void handleDayLights(Query<Write<DirectionalLight>> query, Read<IsDay> is
     if (isDay->value) {
         for (auto [entity, light] : query)
         { 
-            light->intensity = 1.0f;
+            light->intensity = 0.7f;
         }
         env->ambient = {0.5F, 0.5F, 0.5F};
         env->skyGradient[0] = {0.6F, 1.0F, 0.8F};
@@ -285,6 +315,108 @@ static void handleNightLights(Query<Write<SpotLight>, Write<Position>, Write<Rot
     }
 }
 
+
+// -------------------------------------------------------------------------------------
+//                          I have no idea what I'm doing zone
+// -------------------------------------------------------------------------------------
+
+/*
+static void addColliders(Write<State> state, Write<BroadPhaseCollisions> collisions, Commands commands)
+{
+    state->a = commands.create()
+                   .add(BoxCollider{})
+                   .add(LocalToWorld{})
+                   .add(Position{glm::vec3{0.0F, 0.0F, -2.0F}})
+                   .add(Rotation{})
+                   .entity();
+    state->aRotationAxis = glm::sphericalRand(1.0F);
+    collisions->addEntity(state->a);
+
+    state->b = commands.create()
+                   .add(BoxCollider{})
+                   .add(LocalToWorld{})
+                   .add(Position{glm::vec3{0.0F, 0.0F, 2.0F}})
+                   .add(Rotation{})
+                   .entity();
+    state->bRotationAxis = glm::sphericalRand(1.0F);
+    collisions->addEntity(state->b);
+}
+
+static void updateTransform(Write<State> state, Read<Input> input, Query<Write<Position>, Write<Rotation>> query)
+{
+    auto [aPos, aRot] = query[state->a].value();
+    auto [bPos, bRot] = query[state->b].value();
+
+    if (state->collided)
+    {
+        if (input->pressed("reset"))
+        {
+            state->collided = false;
+
+            aPos->vec = glm::vec3{0.0F, 0.0F, -2.0F};
+            aRot->quat = glm::quat{1.0F, 0.0F, 0.0F, 0.0F};
+            state->aRotationAxis = glm::sphericalRand(1.0F);
+
+            bPos->vec = glm::vec3{0.0F, 0.0F, 2.0F};
+            bRot->quat = glm::quat{1.0F, 0.0F, 0.0F, 0.0F};
+            state->bRotationAxis = glm::sphericalRand(1.0F);
+        }
+        return;
+    }
+
+    aRot->quat = glm::rotate(aRot->quat, 0.01F, state->aRotationAxis);
+    aPos->vec += glm::vec3{0.0F, 0.0F, 0.01F};
+
+    bRot->quat = glm::rotate(bRot->quat, 0.01F, state->bRotationAxis);
+    bPos->vec += glm::vec3{0.0F, 0.0F, -0.01F};
+}
+*/
+
+static void carCollision(Query<Read<BoxCollider>, Read<Car>, Read<LocalToWorld>> query, Read<BroadPhaseCollisions> collisions)
+{
+    for (auto [entity, collider, car, transform] : query)
+    {
+        for (auto& entities : collisions->candidates(BroadPhaseCollisions::CollisionType::BoxBox))
+        {
+            BoxCollider otherCollider;
+            LocalToWorld otherTransform;
+            if (entities.first == entity)
+            {
+                auto [collider2, car2, transform2] = query[entities.second].value();
+                otherCollider = *collider2;
+                otherTransform = *transform2;
+            }
+            else if (entities.second == entity)
+            {
+                auto [collider1, car1, transform1] = query[entities.first].value();
+                otherCollider = *collider1;
+                otherTransform = *transform1;
+            }
+            else {
+                break;
+            }
+            
+            CUBOS_INFO("HIT!!!!!!!!!");
+            return;
+        }
+    }
+}
+
+
+static void debugRender(Query<Read<LocalToWorld>, Read<BoxCollider>, Read<ColliderAABB>> query)
+{
+    for (auto [entity, localToWorld, collider, aabb] : query)
+    {
+        cubos::core::gl::Debug::drawWireBox(collider->shape, localToWorld->mat * collider->transform);
+        cubos::core::gl::Debug::drawWireBox(aabb->box(), glm::translate(glm::mat4{1.0}, aabb->center()),
+                                            glm::vec3{1.0, 0.0, 0.0});
+    }
+}
+
+
+
+
+
 int main(int argc, char** argv)
 {
     Cubos cubos{argc, argv};
@@ -292,19 +424,26 @@ int main(int argc, char** argv)
     cubos.addPlugin(rendererPlugin);
     cubos.addPlugin(voxelsPlugin);
     cubos.addPlugin(inputPlugin);
+    cubos.addPlugin(scenePlugin);
     cubos.addComponent<Car>();
     cubos.addComponent<FollowEntity>();
     cubos.addResource<IsDay>();
+
+    cubos.addPlugin(collisionsPlugin);
+    cubos.addPlugin(rendererPlugin);
+    cubos.addPlugin(inputPlugin);
 
     cubos.startupSystem(settings).tagged("cubos.settings");
     cubos.startupSystem(loadInputBindings).tagged("cubos.assets");
     cubos.startupSystem(setup).tagged("cubos.assets").afterTag("cubos.renderer.init");
     cubos.startupSystem(spawnCar).tagged("cubos.assets");
-    cubos.system(move).tagged("car.move");
+    cubos.system(debugRender);
+    cubos.system(carCollision).afterTag("cubos.collisions");
+    cubos.system(move).tagged("car.move").beforeTag("cubos.transform.update");
     cubos.system(followCar).afterTag("car.move");
-    cubos.system(switchDayNight).afterTag("car.move");
-    cubos.system(handleNightLights).afterTag("car.move");
-    cubos.system(handleDayLights).afterTag("car.move");
+    cubos.system(switchDayNight);
+    cubos.system(handleNightLights);
+    cubos.system(handleDayLights);
 
     cubos.run();
 }
