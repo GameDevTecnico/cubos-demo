623af2aae866051aa7a1289309728be13db06c58
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-02-19 13:10:23 +0000

    feat: add bullet hit effect

--- tanks/src/particles/bullet_hit_explosion.hpp
+++ tanks/src/particles/bullet_hit_explosion.hpp
@@ -7,13 +7,13 @@ struct BulletHitExplosion
     CUBOS_REFLECT;
 
     // How long the explosion will last.
-    float duration = 1.5F;
+    float duration = 0.1F;
 
     // Time in seconds between spawning new particles.
-    float particleTime = 0.01F;
+    float particleTime = 0.005F;
 
     // Time in seconds each particle will live.
-    float particleLife = 1.0F;
+    float particleLife = 0.3F;
 
     // Internal timer which spawns new particles when it reaches 0.
     float timer = 0.0F;
