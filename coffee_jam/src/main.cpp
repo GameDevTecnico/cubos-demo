b15da7aedc6fd2a262b5d0a01a0e5662363b5521
Ricardo Antunes <me@riscadoa.com>
2024-04-27 10:25:00 +0100

    feat(coffee-jam): add base code

---
+++ coffee_jam/src/main.cpp
@@ -0,0 +1,71 @@
+#include <imgui.h>
+
+#include <cubos/engine/input/input.hpp>
+#include <cubos/engine/input/bindings.hpp>
+#include <cubos/engine/assets/asset.hpp>
+#include <cubos/engine/assets/assets.hpp>
+#include <cubos/engine/settings/plugin.hpp>
+#include <cubos/engine/scene/scene.hpp>
+#include <cubos/engine/defaults/plugin.hpp>
+#include <cubos/engine/renderer/plugin.hpp>
+#include <cubos/engine/render/lights/environment.hpp>
+#include <cubos/engine/collisions/plugin.hpp>
+#include <cubos/engine/splitscreen/plugin.hpp>
+#include <cubos/engine/imgui/plugin.hpp>
+#include <cubos/engine/utils/free_camera/plugin.hpp>
+
+#include <tesseratos/plugin.hpp>
+
+using namespace cubos::engine;
+
+static const Asset<VoxelPalette> PaletteAsset = AnyAsset("07fb6a49-0e0b-4634-bd67-0fa53a20ae85");
+static const Asset<Scene> MainSceneAsset = AnyAsset("1d2c5bae-f6ec-4986-bc9a-d2863b317c47");
+static const Asset<InputBindings> EditorBindingsAsset = AnyAsset("2f295ec8-aada-41dd-814b-281c5b5859ae");
+static const Asset<InputBindings> PlayerBindingsAsset = AnyAsset("602177be-b7e6-42b4-917e-3947c19e6c19");
+
+int main(int argc, char** argv)
+{
+    Cubos cubos{argc, argv};
+    cubos.plugin(defaultsPlugin);
+    cubos.plugin(freeCameraPlugin);
+    cubos.plugin(tesseratos::plugin);
+
+    // Add plugins
+
+    cubos.startupSystem("configure Assets plugin").tagged(settingsTag).call([](Settings& settings) {
+        settings.setString("assets.io.path", PROJECT_ASSETS_FOLDER);
+        settings.setBool("assets.io.readOnly", false);
+    });
+
+    cubos.startupSystem("load and set the Input Bindings")
+        .tagged(assetsTag)
+        .call([](const Assets& assets, Input& input) {
+            input.bind(*assets.read<InputBindings>(EditorBindingsAsset), 0);
+            input.bind(*assets.read<InputBindings>(PlayerBindingsAsset), 1);
+        });
+
+    cubos.startupSystem("load and set the Voxel Palette")
+        .tagged(assetsTag)
+        .after(rendererInitTag)
+        .call([](const Assets& assets, Renderer& renderer) {
+            // TODO: renderer->setPalette(*assets.read<VoxelPalette>(PaletteAsset));
+        });
+
+    cubos.startupSystem("set environment").call([](RenderEnvironment& environment) {
+        environment.ambient = {0.4F, 0.4F, 0.4F};
+        environment.skyGradient[0] = {0.6F, 1.0F, 0.8F};
+        environment.skyGradient[1] = {0.25F, 0.65F, 1.0F};
+    });
+
+    cubos.startupSystem("load and spawn the Main Scene")
+        .tagged(assetsTag)
+        .call([](Commands cmds, const Assets& assets, Settings& settings, ActiveCameras& cameras) {
+            if (settings.getBool("production", true))
+            {
+                auto builder = cmds.spawn(assets.read(MainSceneAsset)->blueprint);
+                // TODO: cameras.entities[0] = builder.entity("player.camera");
+            }
+        });
+
+    cubos.run();
+}
