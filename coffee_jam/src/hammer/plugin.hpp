8d5606d79de9df51417ee46bfcbd185c84603f32
Ricardo Antunes <me@riscadoa.com>
2024-05-01 10:36:40 +0100

    feat(coffee-jam): add multiple item recipes

--- coffee_jam/src/hammer/plugin.hpp
+++ coffee_jam/src/hammer/plugin.hpp
@@ -14,16 +14,61 @@ namespace demo
         CUBOS_REFLECT;
     };
 
-    /// @brief Component which allows an object to be used to build something else.
+    /// @brief Component for recipe entities.
+    struct Recipe
+    {
+        CUBOS_REFLECT;
+
+        /// @brief Result scene.
+        cubos::engine::Asset<cubos::engine::Scene> result;
+
+        /// @brief Scene root entity name.
+        std::string root;
+    };
+
+    /// @brief Component used to describe ingredient types.
     struct Ingredient
     {
         CUBOS_REFLECT;
 
-        /// @brief Object to spawn in the same position as the entity when construction finishes.
-        cubos::engine::Asset<cubos::engine::Scene> result;
+        /// @brief Ingredient scene.
+        cubos::engine::Asset<cubos::engine::Scene> scene;
 
-        /// @brief Name of the root entity of the result scene.
+        /// @brief Scene root entity name.
         std::string root;
+    };
+
+    /// @brief Component which when added replaces itself by a IsIngredient relation to the Ingredient with the same
+    /// scene asset handle.
+    ///
+    /// This hack is required because we can't link the ingredient type entities directly in the ingredient scenes.
+    struct IsIngredientAsset
+    {
+        CUBOS_REFLECT;
+
+        /// @brief Ingredient scene.
+        cubos::engine::Asset<cubos::engine::Scene> scene;
+    };
+
+    /// @brief Relation between an entity and the ingredient it represents.
+    struct IsIngredient
+    {
+        CUBOS_REFLECT;
+    };
+
+    /// @brief Relation between a recipe and the ingredients it requires.
+    struct Requires
+    {
+        CUBOS_REFLECT;
+
+        /// @brief Amount of the ingredient required.
+        int amount{1};
+    };
+
+    /// @brief Relation between a constructed entity and the recipe that produced it.
+    struct ConstructedFrom
+    {
+        CUBOS_REFLECT;
     };
 
     void hammerPlugin(cubos::engine::Cubos& cubos);
--- coffee_jam/src/holdable/plugin.cpp
+++ coffee_jam/src/holdable/plugin.cpp
@@ -3,6 +3,8 @@
 #include <cubos/core/ecs/reflection.hpp>
 #include <cubos/core/reflection/external/primitives.hpp>
 
+#include <cubos/engine/transform/plugin.hpp>
+
 using namespace cubos::engine;
 
 CUBOS_REFLECT_IMPL(demo::Holdable)
@@ -14,5 +16,19 @@ CUBOS_REFLECT_IMPL(demo::Holdable)
 
 void demo::holdablePlugin(Cubos& cubos)
 {
+    cubos.depends(transformPlugin);
+
     cubos.component<Holdable>();
+
+    cubos.observer("remove stacked entities when bottom is removed")
+        .onRemove<Holdable>()
+        .call([](Commands cmds, Query<Entity> query, Query<Entity, const ChildOf&> children) {
+            for (auto [ent] : query)
+            {
+                for (auto [child, childOf] : children.pin(1, ent))
+                {
+                    cmds.destroy(child);
+                }
+            }
+        });
 }
