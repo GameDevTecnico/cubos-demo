54bbcdfb8d1f60ec9ac563be9355060ece016cbf
joaomanita <joaomanoita3@gmail.com>
2024-05-01 23:02:56 +0100

    add health plugin and turret kills

---
+++ coffee_jam/src/health/plugin.cpp
@@ -0,0 +1,32 @@
+#include "plugin.hpp"
+#include "cubos/engine/prelude.hpp"
+
+#include <cubos/core/ecs/reflection.hpp>
+#include <cubos/core/reflection/external/primitives.hpp>
+#include <cubos/core/reflection/external/string.hpp>
+
+using namespace cubos::engine;
+
+CUBOS_REFLECT_IMPL(demo::Health)
+{
+    return cubos::core::ecs::TypeBuilder<Health>("demo::Health")
+        .withField("hp", &Health::hp)
+        .withField("enemy", &Health::enemy)
+        .build();
+}
+
+void demo::healthPlugin(Cubos& cubos)
+{
+
+    cubos.component<Health>();
+
+    cubos.system("check healthbars").call([](Commands cmds, Query<Entity, const Health&> query) {
+        for (auto [entity, hp] : query)
+        {
+            if (hp.hp <= 0)
+            {
+                cmds.destroy(entity);
+            }
+        }
+    });
+}
---
+++ coffee_jam/src/health/plugin.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cubos/core/reflection/reflect.hpp>
+
+#include <cubos/engine/prelude.hpp>
+
+namespace demo
+{
+    /// @brief Component used for anything that has health.
+    struct Health
+    {
+        CUBOS_REFLECT;
+
+        /// @brief Health points .
+        int hp = 1;
+        int enemy = 1;
+    };
+
+    void healthPlugin(cubos::engine::Cubos& cubos);
+} // namespace demo
