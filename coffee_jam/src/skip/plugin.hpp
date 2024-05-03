00f13c49eb20ccf7de5ab07223f177d878281e87
Ricardo Antunes <me@riscadoa.com>
2024-05-03 20:26:44 +0100

    feat(coffee-jam): reward player on time skip

--- coffee_jam/src/skip/plugin.hpp
+++ coffee_jam/src/skip/plugin.hpp
@@ -3,6 +3,8 @@
 #include <cubos/core/reflection/reflect.hpp>
 
 #include <cubos/engine/prelude.hpp>
+#include <cubos/engine/assets/asset.hpp>
+#include <cubos/engine/scene/scene.hpp>
 
 namespace demo
 {
@@ -10,6 +12,12 @@ namespace demo
     struct Skip
     {
         CUBOS_REFLECT;
+
+        /// @brief Reward scene.
+        cubos::engine::Asset<cubos::engine::Scene> reward;
+
+        /// @brief Scene root entity name.
+        std::string root;
     };
 
     void skipPlugin(cubos::engine::Cubos& cubos);
