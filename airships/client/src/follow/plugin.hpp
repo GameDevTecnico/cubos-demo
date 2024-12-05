93ed8d920c628952f5c007f2e23d6c815b14eaa5
Ricardo Antunes <me@riscadoa.com>
2024-12-05 14:55:19 +0100

    feat(airships): add follow height

--- airships/client/src/follow/plugin.hpp
+++ airships/client/src/follow/plugin.hpp
@@ -23,8 +23,12 @@ namespace airships::client
         /// @brief Optional entity to base the yaw and position on.
         cubos::engine::Entity basedOn{};
 
+        /// @brief Height offset from the target entity.
+        float height{0.0F};
+
         /// @brief Point the entity is actually following, which follows the target entity.
         glm::vec3 center{0.0F};
+
         bool initialized{false};
     };
 
