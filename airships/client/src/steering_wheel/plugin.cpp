715397b4ed37da5f623a5c30fabd5582939175a7
Ricardo Antunes <me@riscadoa.com>
2024-12-15 20:24:26 +0100

    feat(airships): auto rotate steering wheel when out of bounds

--- airships/client/src/steering_wheel/plugin.cpp
+++ airships/client/src/steering_wheel/plugin.cpp
@@ -23,6 +23,8 @@ CUBOS_REFLECT_IMPL(airships::client::SteeringWheel)
         .withField("turnAngle", &SteeringWheel::turnAngle)
         .withField("turnSpeed", &SteeringWheel::turnSpeed)
         .withField("targetLerpFactor", &SteeringWheel::targetLerpFactor)
+        .withField("startAutoTurnDistance", &SteeringWheel::startAutoTurnDistance)
+        .withField("endAutoTurnDistance", &SteeringWheel::endAutoTurnDistance)
         .build();
 }
 
@@ -77,16 +79,35 @@ void airships::client::steeringWheelPlugin(Cubos& cubos)
 
     cubos.system("update SteeringWheel entities")
         .call([](const DeltaTime& dt, Input& input,
-                 Query<const SteeringWheelHead&, Rotation&, const ChildOf&, const InterpolationOf&, SteeringWheel&,
-                       const ChildOf&, Drivable&>
+                 Query<const LocalToWorld&, const SteeringWheelHead&, Rotation&, const ChildOf&, const InterpolationOf&,
+                       SteeringWheel&, const ChildOf&, Drivable&>
                      query) {
-            for (auto [head, rotation, childOf1, interpolationOf, wheel, childOf2, drivable] : query)
+            for (auto [ltw, head, rotation, childOf1, interpolationOf, wheel, childOf2, drivable] : query)
             {
                 float turnInput = wheel.player == -1 ? 0.0F : input.axis(wheel.turnAxis.c_str(), wheel.player);
 
                 // Update target turn angle
                 wheel.targetTurnAngle += turnInput * wheel.turnSpeed * dt.value();
                 wheel.targetTurnAngle = glm::clamp(wheel.targetTurnAngle, -wheel.maxTurnAngle, wheel.maxTurnAngle);
+
+                // Turn wheel automatically if outside of the auto turn distance
+                auto currentPosition = ltw.worldPosition();
+                auto distanceToCenter =
+                    glm::distance(glm::vec3{currentPosition.x, 0.0F, currentPosition.z}, glm::vec3{0.0F, 0.0F, 0.0F});
+                if (distanceToCenter > wheel.startAutoTurnDistance)
+                {
+                    auto ratio = (distanceToCenter - wheel.startAutoTurnDistance) /
+                                 (wheel.endAutoTurnDistance - wheel.startAutoTurnDistance);
+                    ratio = glm::clamp(ratio, 0.0F, 1.0F);
+
+                    // Calculate the angle between the current drivable direction and the direction to the center
+                    auto forward = glm::vec2(glm::sin(glm::radians(drivable.yaw)), glm::cos(glm::radians(drivable.yaw)));
+                    auto toCenter = glm::normalize(glm::vec2{-currentPosition.x, -currentPosition.z});
+                    auto diff = glm::degrees(glm::atan(toCenter.y, toCenter.x) - glm::atan(forward.y, forward.x)) / 180.0F;
+
+                    // Turn the wheel so that the drivable starts turning back
+                    wheel.targetTurnAngle = glm::mix(wheel.targetTurnAngle, diff * wheel.maxTurnAngle, ratio);
+                }
 
                 // Smoothly turn the actual wheel towards the target turn angle
                 wheel.turnAngle = glm::mix(wheel.turnAngle, wheel.targetTurnAngle,
