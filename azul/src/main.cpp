f8bd991036461a9a174562607c7de21530b3c850
Ricardo Antunes <me@riscadoa.com>
2025-03-22 18:46:24 +0000

    feat: setup azul jam dir

---
+++ azul/src/main.cpp
@@ -0,0 +1,48 @@
+#include <imgui.h>
+
+#include <cubos/engine/assets/plugin.hpp>
+#include <cubos/engine/settings/plugin.hpp>
+#include <cubos/engine/scene/scene.hpp>
+#include <cubos/engine/defaults/plugin.hpp>
+#include <cubos/engine/render/voxels/palette.hpp>
+#include <cubos/engine/render/lights/environment.hpp>
+#include <cubos/engine/collisions/plugin.hpp>
+#include <cubos/engine/imgui/plugin.hpp>
+
+#include <glm/gtx/color_space.hpp>
+
+using namespace cubos::engine;
+
+static const Asset<VoxelPalette> PaletteAsset = AnyAsset("96e982ca-a18e-49e0-bf0b-2ddbbcf4d4cb");
+static const Asset<Scene> MainSceneAsset = AnyAsset("b36dfc06-4ec3-4658-ba67-f8cb3ec787c0");
+
+int main(int argc, char** argv)
+{
+    Cubos cubos{argc, argv};
+    cubos.plugin(defaultsPlugin);
+
+    // Add game plugins
+    // TODO: ???
+
+    cubos.startupSystem("configure project").before(settingsTag).call([](Settings& settings) {
+        settings.setString("assets.app.osPath", APP_ASSETS_PATH);
+        settings.setString("assets.builtin.osPath", BUILTIN_ASSETS_PATH);
+        settings.setBool("cubos.renderer.screenPicking.enabled", false);
+    });
+
+    cubos.startupSystem("set the Voxel Palette").tagged(assetsTag).call([](Assets& assets, RenderPalette& palette) {
+        palette.asset = assets.create(*assets.read(PaletteAsset));
+    });
+
+    cubos.startupSystem("set environment").call([](RenderEnvironment& environment) {
+        environment.ambient = {0.4F, 0.4F, 0.4F};
+        environment.skyGradient[0] = {0.6F, 1.0F, 0.8F};
+        environment.skyGradient[1] = {0.25F, 0.65F, 1.0F};
+    });
+
+    cubos.startupSystem("load and spawn the Main Scene")
+        .tagged(assetsTag)
+        .call([](Commands cmds, const Assets& assets) { cmds.spawn(assets.read(MainSceneAsset)->blueprint); });
+
+    cubos.run();
+}
