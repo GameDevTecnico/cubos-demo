f5b609789199c5dbc68153f79436f2d3cf681109
Ricardo Antunes <me@riscadoa.com>
2025-03-30 10:21:01 +0100

    feat: add sailor death animation

--- azul/src/sailor_death/plugin.cpp
+++ azul/src/sailor_death/plugin.cpp
@@ -3,11 +3,14 @@
 #include "../health/plugin.hpp"
 
 #include <cubos/core/ecs/reflection.hpp>
-#include <cubos/core/reflection/traits/enum.hpp>
 #include <cubos/core/reflection/external/primitives.hpp>
+#include <cubos/core/reflection/external/glm.hpp>
 #include <cubos/core/reflection/external/string.hpp>
 
 #include <cubos/engine/prelude.hpp>
+#include <cubos/engine/transform/plugin.hpp>
+
+#include <random>
 
 using namespace cubos::engine;
 
@@ -16,20 +19,73 @@ CUBOS_REFLECT_IMPL(demo::SailorDeath)
     return cubos::core::ecs::TypeBuilder<SailorDeath>("demo::SailorDeath").withField("hp", &SailorDeath::hp).build();
 }
 
+CUBOS_REFLECT_IMPL(demo::SailorDeathAnimation)
+{
+    return cubos::core::ecs::TypeBuilder<SailorDeathAnimation>("demo::SailorDeathAnimation")
+        .withField("time", &SailorDeathAnimation::time)
+        .withField("speed", &SailorDeathAnimation::speed)
+        .withField("duration", &SailorDeathAnimation::duration)
+        .withField("source", &SailorDeathAnimation::source)
+        .withField("target", &SailorDeathAnimation::target)
+        .withField("maxHeight", &SailorDeathAnimation::maxHeight)
+        .build();
+}
+
 void demo::sailorDeathPlugin(Cubos& cubos)
 {
     cubos.depends(healthPlugin);
     cubos.depends(destroyTreePlugin);
+    cubos.depends(transformPlugin);
 
     cubos.relation<SailorDeath>();
+    cubos.component<SailorDeathAnimation>();
 
-    cubos.system("kill sailors").call([](Commands cmds, Query<Entity, const SailorDeath&, const Health&> query) {
-        for (auto [ent, death, health] : query)
-        {
-            if (health.hp <= death.hp)
+    cubos.system("kill sailors")
+        .call([](Commands cmds,
+                 Query<Entity, Position&, Rotation&, Scale&, LocalToWorld&, const SailorDeath&, const Health&, Entity>
+                     query,
+                 Query<const ChildOf&, Entity> parentQuery) {
+            for (auto [ent, pos, rot, scale, ltw, death, health, playerEnt] : query)
             {
-                cmds.add(ent, DestroyTree{});
+                if (health.hp <= death.hp)
+                {
+                    auto parent = parentQuery.pin(0, ent).first();
+                    auto [_1, parentEnt] = *parent;
+                    pos.vec = ltw.worldPosition();
+                    rot.quat = ltw.worldRotation();
+                    scale.factor = ltw.worldScale();
+
+                    auto rightDir = rot.quat * glm::vec3{1.0F, 0.0F, 0.0F};
+                    auto sign = (rand() % 2) ? -1.0F : 1.0F;
+
+                    auto source = glm::vec2(pos.vec.x, pos.vec.z);
+                    auto target = source + glm::vec2{rightDir.x, rightDir.z} * sign;
+
+                    cmds.unrelate<ChildOf>(ent, parentEnt);
+                    cmds.unrelate<SailorDeath>(ent, playerEnt);
+                    cmds.add(ent, SailorDeathAnimation{
+                                      .source = source,
+                                      .target = target,
+                                      .initialHeight = pos.vec.y,
+                                  });
+                }
             }
-        }
-    });
+        });
+
+    cubos.system("do sailor death animation")
+        .before(transformUpdateTag)
+        .call([](Commands cmds, const DeltaTime& dt, Query<Entity, Position&, SailorDeathAnimation&> query) {
+            for (auto [ent, pos, anim] : query)
+            {
+                anim.time += dt.value() * anim.speed;
+                if (anim.time >= anim.duration)
+                {
+                    cmds.add(ent, DestroyTree{});
+                }
+
+                glm::vec2 posXZ = glm::mix(anim.source, anim.target, anim.time);
+                auto currentY = anim.initialHeight + anim.maxHeight * (1 - glm::pow(2.0 * anim.time - 1.0, 2.0));
+                pos.vec = {posXZ.x, currentY, posXZ.y};
+            }
+        });
 }
--- azul/src/sailor_death/plugin.hpp
+++ azul/src/sailor_death/plugin.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glm/vec2.hpp>
+
 #include <cubos/core/reflection/reflect.hpp>
 
 #include <cubos/engine/prelude.hpp>
@@ -13,8 +15,35 @@ namespace demo
     {
         CUBOS_REFLECT;
 
-        /// @brief HP level at which the sailor dies. 
+        /// @brief HP level at which the sailor dies.
         int hp = 0;
+    };
+
+    /// @brief Holds sailor death animation data.
+    struct SailorDeathAnimation
+    {
+        CUBOS_REFLECT;
+
+        /// @brief Progress where 0 is the source and 1 is the target.
+        float time{0.0F};
+
+        /// @brief Speed of the animation.
+        float speed{1.0F};
+
+        /// @brief Duration of the animation.
+        float duration{5.0F};
+
+        /// @brief Source position.
+        glm::vec2 source{};
+
+        /// @brief Target position.
+        glm::vec2 target{};
+
+        /// @brief Initial trajectory height.
+        float initialHeight{0.0F};
+
+        /// @brief Maximum trajectory height.
+        float maxHeight{1.0F};
     };
 
     void sailorDeathPlugin(cubos::engine::Cubos& cubos);
