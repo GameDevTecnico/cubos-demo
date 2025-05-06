30689a9c10c5121b8bd3ef889b8bbe6b87241e9a
Ricardo Antunes <me@riscadoa.com>
2025-05-06 16:20:44 +0100

    feat: add new tile system

--- coffee/src/map_generator/plugin.hpp
+++ coffee/src/map_generator/plugin.hpp
@@ -4,37 +4,21 @@
 #include <cubos/engine/assets/asset.hpp>
 #include <cubos/engine/scene/scene.hpp>
 
-#include <cubos/core/reflection/external/glm.hpp>
-#include <cubos/core/reflection/external/vector.hpp>
-
 #include <glm/glm.hpp>
 
 namespace coffee
 {
-    struct MapConnector
-    {
-        CUBOS_REFLECT;
-
-        glm::vec3 position;
-        int rotation{0};
-    };
-
-    struct MapTile
-    {
-        CUBOS_REFLECT;
-
-        cubos::engine::Asset<cubos::engine::Scene> scene;
-        std::vector<MapConnector> connectors;
-    };
-
     struct MapGenerator
     {
         CUBOS_REFLECT;
 
-        std::size_t length{5};
-        MapTile start;
-        std::vector<MapTile> middle;
-        MapTile end;
+        glm::ivec2 tileSize{};
+        std::size_t trackLength{};
+        cubos::engine::Asset<cubos::engine::Scene> startTileScene;
+        cubos::engine::Asset<cubos::engine::Scene> endTileScene;
+        cubos::engine::Asset<cubos::engine::Scene> straightTileScene;
+        cubos::engine::Asset<cubos::engine::Scene> curveLeftTileScene;
+        cubos::engine::Asset<cubos::engine::Scene> curveRightTileScene;
     };
 
     void mapGeneratorPlugin(cubos::engine::Cubos& cubos);
