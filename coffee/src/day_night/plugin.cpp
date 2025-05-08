0036c364affe9c87351052a5ed1b81d50eae0383
Ricardo Antunes <me@riscadoa.com>
2025-05-08 08:39:21 +0100

    feat: improve day light environment

--- coffee/src/day_night/plugin.cpp
+++ coffee/src/day_night/plugin.cpp
@@ -1,6 +1,7 @@
 #include "plugin.hpp"
 #include "../progression/plugin.hpp"
 
+#include <cubos/core/ecs/reflection.hpp>
 #include <cubos/engine/render/lights/environment.hpp>
 #include <cubos/engine/render/lights/directional.hpp>
 #include <cubos/engine/render/lights/spot.hpp>
@@ -10,32 +11,47 @@
 
 using namespace cubos::engine;
 
-namespace
+CUBOS_REFLECT_IMPL(demo::DayNightManager)
 {
-    struct State
-    {
-        CUBOS_ANONYMOUS_REFLECT(State);
+    return cubos::core::ecs::TypeBuilder<DayNightManager>("demo::DayNightManager")
+        .withField("state", &DayNightManager::state)
+        .withField("speed", &DayNightManager::speed)
+        .withField("nightEnvironment", &DayNightManager::nightEnvironment)
+        .withField("nightSunLight", &DayNightManager::nightSunLight)
+        .withField("nightCarLightIntensity", &DayNightManager::nightCarLightIntensity)
+        .withField("dayEnvironment", &DayNightManager::dayEnvironment)
+        .withField("daySunLight", &DayNightManager::daySunLight)
+        .withField("dayCarLightIntensity", &DayNightManager::dayCarLightIntensity)
+        .build();
+}
 
-        /// @brief Current state of the day-night cycle, where 0.0 is full day and 1.0 is full night.
-        float state = 0.0F;
-
-        /// @brief How fast the state changes.
-        float speed = 0.75F;
-    };
-} // namespace
+CUBOS_REFLECT_IMPL(demo::CarLight)
+{
+    return cubos::core::ecs::TypeBuilder<CarLight>("demo::CarLight").build();
+}
 
 void demo::dayNightPlugin(Cubos& cubos)
 {
     cubos.depends(lightsPlugin);
     cubos.depends(progressionPlugin);
 
-    cubos.resource<State>();
+    cubos.component<DayNightManager>();
+    cubos.component<CarLight>();
 
-    cubos.system("update RenderEnvironment")
-        .call([](const Progression& progression, const DeltaTime& dt, State& state,
-                 RenderEnvironment& renderEnvironment, Query<DirectionalLight&> sun, Query<SpotLight&> lanterns) {
+    cubos.system("update DayNightManager")
+        .call([](const Progression& progression, const DeltaTime& dt, Query<DayNightManager&> managers,
+                 RenderEnvironment& renderEnvironment, Query<DirectionalLight&> sun,
+                 Query<SpotLight&, CarLight&> carLights) {
+            CUBOS_ASSERT(managers.count() <= 1, "Only one DayNightManager at a time is allowed.");
+            auto match = managers.first();
+            if (!match)
+            {
+                return;
+            }
+            auto& [manager] = *match;
+
             // Gradually change the state of the day-night cycle.
-            float alpha = 1.0F - glm::pow(0.5F, dt.value() * state.speed);
+            float alpha = 1.0F - glm::pow(0.5F, dt.value() * manager.speed);
             float desiredState;
 
             if (progression.timeOfDay < progression.dayDuration * 0.5F)
@@ -47,23 +63,28 @@ void demo::dayNightPlugin(Cubos& cubos)
                 desiredState = 1.0F;
             }
 
-            state.state = glm::mix(state.state, desiredState, alpha);
+            manager.state = glm::mix(manager.state, desiredState, alpha);
 
             // Update the sun's intensity.
             for (auto [light] : sun)
             {
-                light.intensity = glm::mix(0.75F, 0.0F, state.state);
+                light.color = glm::mix(manager.daySunLight.color, manager.nightSunLight.color, manager.state);
+                light.intensity =
+                    glm::mix(manager.daySunLight.intensity, manager.nightSunLight.intensity, manager.state);
             }
 
-            // Update spot lights intensity.
-            for (auto [light] : lanterns)
+            // Update car lights intensity.
+            for (auto [light, carLight] : carLights)
             {
-                light.intensity = glm::mix(0.0F, 1.5F, state.state);
+                light.intensity = glm::mix(manager.dayCarLightIntensity, manager.nightCarLightIntensity, manager.state);
             }
 
             // Update the render environment.
-            renderEnvironment.ambient = glm::mix(glm::vec3(0.4F), glm::vec3(0.1F), state.state);
-            renderEnvironment.skyGradient[0] = glm::mix({0.6F, 1.0F, 0.8F}, glm::vec3(0.1F), state.state);
-            renderEnvironment.skyGradient[1] = glm::mix({0.6F, 1.0F, 0.8F}, glm::vec3(0.1F), state.state);
+            renderEnvironment.ambient =
+                glm::mix(manager.dayEnvironment.ambient, manager.nightEnvironment.ambient, manager.state);
+            renderEnvironment.skyGradient[0] =
+                glm::mix(manager.dayEnvironment.skyGradient[0], manager.nightEnvironment.skyGradient[0], manager.state);
+            renderEnvironment.skyGradient[1] =
+                glm::mix(manager.dayEnvironment.skyGradient[1], manager.nightEnvironment.skyGradient[1], manager.state);
         });
 }
