ca5d982d508d632fd18c228337cd00a0f8a1c66f
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2025-05-10 12:32:46 +0100

    feat: give bonus score

--- coffee/src/score/plugin.cpp
+++ coffee/src/score/plugin.cpp
@@ -1,5 +1,4 @@
 #include "plugin.hpp"
-#include "../toilet_paper/plugin.hpp"
 
 #include <cubos/core/ecs/reflection.hpp>
 #include <cubos/core/reflection/external/primitives.hpp>
@@ -14,23 +13,5 @@ CUBOS_REFLECT_IMPL(coffee::PlayerScores)
 
 void coffee::scorePlugin(Cubos& cubos)
 {
-    cubos.depends(toiletPaperPlugin);
-
     cubos.resource<PlayerScores>();
-
-    cubos.system("give score to player holding toilet paper")
-        .call([](Query<ToiletPaper&> query, const DeltaTime& dt, PlayerScores& scores) {
-            scores.scoreTimer -= dt.value();
-            for (auto [toiletPaper] : query)
-            {
-                if (toiletPaper.player != -1 && scores.scoreTimer <= 0.0F)
-                {
-                    scores.scores[toiletPaper.player - 1] += 1;
-                }
-            }
-            if (scores.scoreTimer <= 0.0F)
-            {
-                scores.scoreTimer = 0.1F;
-            }
-        });
 }
